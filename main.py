b985b5d0dc42bbaf687a720c172323eb533b04fa
marbiru <code@uribram.com>
2015-12-01 15:36:38 +0700

    my_pick_array now nested lists
    my_pick_array now a nested list of player's pick and the percentage win
    probability

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
-myPickArray = [];
+my_pick_array = [];
 
-for x in range(2,27):
-	myPickArray.append(x)
+for x in range(2,28):
+	my_pick_array.append([x,""])
 
-print myPickArray
+print my_pick_array
 
-def win_chance(myPick):
+def win_chance(my_pick):
     
-    myWins = 0
+    my_wins = 0
 
-    for randomPick in range(2,28):
+    for random_pick in range(2,28):
         # Is this the right way to deal with Draws?
-        if randomPick == myPick:
-        	myWins += 0.5
-        elif randomPick % myPick == 0:
-            myWins += 0
-        elif myPick % randomPick == 0:
-        	myWins += 1
-        elif myPick < randomPick:
-        	myWins += 1
-        elif randomPick < myPick:
-        	myWins += 0
+        if random_pick == my_pick:
+        	my_wins += 0.5
+        elif random_pick % my_pick == 0:
+            my_wins += 0
+        elif my_pick % random_pick == 0:
+        	my_wins += 1
+        elif my_pick < random_pick:
+        	my_wins += 1
+        elif random_pick < my_pick:
+        	my_wins += 0
 
-    return myWins
+    return my_wins
 
-print win_chance(2)
+for x in range(2,28):
+	my_pick_array[x-2][1] = win_chance(x)
+
+print my_pick_array
