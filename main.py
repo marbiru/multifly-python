b82697d263f0f59a24e0f39c8a88161f3399fdcf
marbiru <code@uribram.com>
2015-12-01 22:25:50 +0700

    pops computer's pick & recalculates probabilities
    correctly pops the computer's pick from random_pick_array then
    recalculates probabilities for human's next go

--- main.py
+++ main.py
@@ -6,15 +6,14 @@ for x in range(2,28):
 random_pick_array = []
 
 for x in range(2,28):
-    random_pick_array.append([x])
+    random_pick_array.append(x)
 
 def win_chance(my_pick):
     
     my_wins = 0
 
     #instead of "range" this needs to use an array as its input and we pop
-    for random_pick in range(2,28):
-        # Is this the right way to deal with Draws?
+    for random_pick in random_pick_array:
         if random_pick == my_pick:
         	my_wins += 0.5
         elif random_pick % my_pick == 0:
@@ -43,3 +42,14 @@ computer_played = int(raw_input('Computer played [number]?\n'))
 random_pick_array.pop(computer_played - 2)
 
 print random_pick_array
+
+for x in range(2,28):
+    my_pick_array[x-2][1] = win_chance(x)
+
+def sort_by_prob(s):
+    return s[1]
+
+my_pick_array_sorted = sorted(my_pick_array, key=sort_by_prob, reverse=True)
+
+print my_pick_array_sorted
+
