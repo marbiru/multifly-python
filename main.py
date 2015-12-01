1e42f43503c3d01f309bcd414d9fc1bd79d5b7c8
marbiru <code@uribram.com>
2015-12-01 15:15:55 +0700

    calculates myWins for first pick correctly
    currently just for individually-specified First Picks

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 myPickArray = [];
 
-for x in xrange(2,27):
+for x in range(2,27):
 	myPickArray.append(x)
 
 print myPickArray
+
+def win_chance(myPick):
+    
+    myWins = 0
+
+    for randomPick in range(2,28):
+        # Is this the right way to deal with Draws?
+        if randomPick == myPick:
+        	myWins += 0.5
+        elif randomPick % myPick == 0:
+            myWins += 0
+        elif myPick % randomPick == 0:
+        	myWins += 1
+        elif myPick < randomPick:
+        	myWins += 1
+        elif randomPick < myPick:
+        	myWins += 0
+
+    return myWins
+
+print win_chance(2)
