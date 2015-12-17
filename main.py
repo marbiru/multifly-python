ca42493d859b992e938cecac3ac03172981023c6
marbiru <code@uribram.com>
2015-12-17 14:08:21 +0700

    sanitizes user input
    checks user-input number (the computer's last move) is in correct
    range, and checks it hasn't been played before.
    
    Does not sanitize for non-numbers, which still throws an error

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def calculate_probabilities():
     my_pick_list_sorted = sorted(my_pick_list, key=sort_by_prob, reverse=True)
 
     print "\n Your next move:"
-    print my_pick_list_sorted[0]
+    print my_pick_list_sorted[0][:-1]
     print "\n Your previous moves:"
     print my_previous_moves
     print "\n Computer's previous moves:"
@@ -61,6 +61,13 @@ def calculate_probabilities():
 def computers_move():
 
     user_input = int(raw_input('\nWhat number did the computer play?\n'))
+
+    if user_input in computers_previous_moves:
+         user_input = int(raw_input('\nYou\'ve entered that previously! What number did the computer play this time?\n'))
+    elif user_input in range(2,27):
+        return user_input
+    else: 
+        user_input = int(raw_input('\nThat doesn\'t seem right. What number did the computer play?\n'))
     
     computers_previous_moves.append(user_input)
 
